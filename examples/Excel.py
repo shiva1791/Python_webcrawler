26bf700316b9608edb63a2dce80617a45a737195
shiva1791 <shiva1791@gmail.com>
2015-10-07 00:40:32 +0530

    Monthly defects based on priorities of the defect
    Get monthly count of defect differentiated as total defects per month
    and total p1, p2,p3 defect

--- examples/Excel.py
+++ examples/Excel.py
@@ -59,6 +59,9 @@ class excel:
         monthly_defect = []
         month_year_name = []
         defect_count_month = []
+        priority_1 = []
+        priority_2 = []
+        priority_3 = []
         for i in range(len(defectdetails)):
             for year in range(2015,2017,1):
                 for month_val in range(1,13,1):
@@ -76,29 +79,57 @@ class excel:
             for month_val in range(1,13,1):
                 value_to_search = str((str(months[month_val-1]) + ' '+str(year)))
                 count =0
+                p1,p2,p3=0,0,0
+                print('-------')
                 for i in range(len(defectdetails)):
                     if value_to_search in defectdetails[i]:
                         count +=1
+                        print(count)
+                        if defectdetails[i][1] ==1:
+                            p1 +=1
+                        elif defectdetails[i][1] ==2:
+                            p2+=1
+                        elif defectdetails[i][1] ==3:
+                            p3+=1
+                        else:
+                            pass
                 if count>0:
                     month_year_name.append(value_to_search)
                     defect_count_month.append(count)
+                if p1 >=0 or p2>=0 or p3>=0:
+                    priority_1.append(p1)
+                    priority_2.append(p2)
+                    priority_3.append(p3)
             pass
 
         for i in range(len(month_year_name)):
-            monthly_defect.append([month_year_name[i],defect_count_month[i]])
+            monthly_defect.append([month_year_name[i],defect_count_month[i],priority_1[i],priority_2[i],priority_3[i]])
 
         for i in range(len(monthly_defect)):
+            pass
             print(monthly_defect[i])
+        excel.writeinsheet(self,monthly_defect)
+
+        #For counting defects monthly based on priority
+
+
+
 
     def writeinsheet(self,anylist):
         anylist = anylist
         wb = openpyxl.load_workbook('Defect.xlsx')
         sheetnames = wb.get_sheet_names() # to get sheet names in the excel
         sheet = wb.get_sheet_by_name(sheetnames[2])
+        wb.remove_sheet(sheet)
+
+        wb.create_sheet(2,title='Temp')
+        wb.save('Defect.xlsx')
+        sheet = wb.get_sheet_by_name('Temp')
 
         for rows in range(len(anylist)):
             for columns in range(len(anylist[rows])):
                 sheet.cell(row=rows+1,column=columns+1, value=str(anylist[rows][columns]))
+                print(anylist[rows][columns])
         wb.save('Defect.xlsx')
 
 
